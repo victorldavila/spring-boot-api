b720e8fc3b80b01022f20793ffc03a0a5ae4809e
Victor D'Avila <victorldavila@gmail.com>
2024-03-27 18:37:30 -0300

    add gradle file

---
+++ lib/products-api/build.gradle.kts
@@ -0,0 +1,47 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import com.finapp.buildsrc.ApiGroup
+import com.finapp.buildsrc.ApiVersion
+import com.finapp.buildsrc.userLibImplementation
+
+plugins {
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+
+    kotlin("jvm")
+    kotlin("plugin.spring")
+}
+
+group = ApiGroup.Lib.userInterface
+version = ApiVersion.Lib.user
+
+
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+    mavenCentral()
+}
+
+tasks.jar {
+    enabled = true
+}
+
+tasks.bootJar {
+    enabled = false
+}
+
+
+dependencies {
+    implementation(project(":lib:core"))
+
+    userLibImplementation()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
