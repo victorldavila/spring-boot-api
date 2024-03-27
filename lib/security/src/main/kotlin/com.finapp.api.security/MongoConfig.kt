f11be45c488229961bb044ff7196bc492b6f9316
Victor D'Avila <victorldavila@gmail.com>
2024-03-27 19:53:12 -0300

    remove comments

--- lib/security/src/main/kotlin/com.finapp.api.security/MongoConfig.kt
+++ lib/security/src/main/kotlin/com.finapp.api.security/MongoConfig.kt
@@ -1,17 +1,11 @@
 package com.finapp.api.security
 
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.data.domain.AuditorAware
-import org.springframework.data.mongodb.config.EnableMongoAuditing
 import org.springframework.data.mongodb.config.EnableReactiveMongoAuditing
 
 
 @Configuration
 @EnableReactiveMongoAuditing
 class MongoConfig {
-//    @Bean
-//    fun myAuditorProvider(): AuditorAware<AuditableUser> {
-//        return AuditorAwareImpl()
-//    }
+
 }
