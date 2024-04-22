fe41c55e3d61e93595adca724c5c9cfaf09b2156
Victor D'Avila <victorldavila@gmail.com>
2024-04-21 21:48:52 -0300

    fix listener to delete action

--- lib/security/src/main/kotlin/com.finapp.api.security/UserCascadeDeleteMongoEventListener.kt
+++ lib/security/src/main/kotlin/com.finapp.api.security/UserCascadeDeleteMongoEventListener.kt
@@ -4,7 +4,7 @@ import com.finapp.api.role.repository.RoleRepository
 import com.finapp.api.users.data.User
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.mongodb.core.mapping.event.AbstractMongoEventListener
-import org.springframework.data.mongodb.core.mapping.event.BeforeConvertEvent
+import org.springframework.data.mongodb.core.mapping.event.BeforeDeleteEvent
 import org.springframework.stereotype.Component
 
 @Component
@@ -14,8 +14,8 @@ class UserCascadeDeleteMongoEventListener(
     @Autowired
     private lateinit var roleRepository: RoleRepository
 
-    override fun onBeforeConvert(event: BeforeConvertEvent<User>) {
-        val userId = event.source.id
+    override fun onBeforeDelete(event: BeforeDeleteEvent<User>) {
+        val userId = event.source.getObjectId("_id")
 
         roleRepository.deleteRoleByUserId(userId)
             .subscribe()
