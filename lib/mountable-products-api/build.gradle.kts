8a64a3ab1ca1d3557b820bccaaaa9fd84ea70484
Victor D'Avila <victorldavila@gmail.com>
2024-04-22 22:54:33 -0300

    add module gradle

---
+++ lib/mountable-products-api/build.gradle.kts
@@ -0,0 +1,48 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import com.finapp.buildsrc.ApiGroup
+import com.finapp.buildsrc.ApiVersion
+import com.finapp.buildsrc.baseLibsImplementation
+import com.finapp.buildsrc.userLibImplementation
+
+plugins {
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+
+    kotlin("jvm")
+    kotlin("plugin.spring")
+}
+
+group = ApiGroup.Lib.MOUNTABLE_PRODUCTS_API
+version = ApiVersion.Lib.MOUNTABLE_PRODUCTS_API
+
+
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+    mavenCentral()
+}
+
+tasks.jar {
+    enabled = true
+}
+
+tasks.bootJar {
+    enabled = false
+}
+
+
+dependencies {
+    implementation(project(":lib:core"))
+
+    baseLibsImplementation()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
